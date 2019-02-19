585e1b9362a30afe3d79b3610b54ff52f016c7d7
yossigor <yossigor@gmail.com>
2019-02-19 14:28:07 +0200

    now the main.py can run in python3

--- dst/output.py
+++ dst/output.py
@@ -2,15 +2,15 @@ from dst.libraries.dictionary_filter import dictionary_interactive
 
 def console(in_queue, config):
     for s_line in config.SPLASHSCREEN:
-        print s_line
+        print(s_line)
     output = []
     for idx, pred in iter(in_queue.get, None):
         output.append((idx,pred))
-    output.sort(key = lambda (x,y): x);
-    print "PREDICTIONS"
-    print ""
+    output.sort(key = lambda k: k[0]);
+    print("PREDICTIONS")
+    print("")
     for i, p in output:
-        print "{} - {}".format(i, p)
+        print("{} - {}".format(i, p))
     dictionary_interactive([y for (x,y) in output], config)
 
     
